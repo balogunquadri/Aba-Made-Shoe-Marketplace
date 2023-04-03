83644c7957d04de76c898ff3210ea54bd3b85ac2
Phantonce <yes201045@gmail.com>
2023-04-03 07:44:07 +0100

    Navbar Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { HeartIcon, MagnifyingGlassIcon, ShoppingBagIcon } from '@heroicons/react/24/outline'
 import logo from '../assets/logo.png';
 
 
 
 const Navbar = () => {
+
+    const [navState, setNavState] = useState(false);
+    const onNavScroll = () => {
+        if (window.scrollY > 30) {
+            setNavState(true);
+        } else {
+            setNavState(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', onNavScroll);
+
+
+        return () => {
+            window.removeEventListener('scroll', onNavScroll);
+        }
+    }, []);
     return (
         <>
-            <header className={`absolute top-7 left-0 right-0 opacity-100 z-50`}>
+           <header className={
+        !navState ? 'absolute top-7 left-0 right-0 opacity-100 z-50' : 'fixed top-0 left-0 right-0 h-[9vh] flex items-center justify-center opacity-100 z-[200] blur-effect-theme'
+      }>
                 <nav className='flex items-center justify-between nike-container '>
                     <div className='flex items-center'>
                         <img
@@ -26,8 +46,9 @@ const Navbar = () => {
                         </li>
 
                         <li className='grid items-center'>
-                            <button type='button'>
+                            <button type='button' className='border-none outline-none active:scale-110 transition-all duration-300 relative'>
                                 <ShoppingBagIcon className='icon-style' />
+                                <div className={`absolute top-4 right-0 bg-white text-slate-900 shadow shadow-slate-100 w-4 h-4 text-[0.65rem] leading-tight font-medium rounded-full flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-300 `}>0</div>
                             </button>
                         </li>
                     </ul>
