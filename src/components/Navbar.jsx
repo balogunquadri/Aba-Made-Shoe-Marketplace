7cbc2fa2b887608cbf4d699a7ad3555ae58e08fa
Phantonce <yes201045@gmail.com>
2023-04-05 12:36:24 +0100

    cart updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { selectTotalQTY, setOpenCart } from '../app/CartSlice.js';
 import { HeartIcon, MagnifyingGlassIcon, ShoppingBagIcon } from '@heroicons/react/24/outline'
 import logo from '../assets/logo.png';
 
@@ -7,6 +9,15 @@ import logo from '../assets/logo.png';
 const Navbar = () => {
 
     const [navState, setNavState] = useState(false);
+    const dispatch = useDispatch();
+
+    const onCartToggle = () => {
+        dispatch(setOpenCart({
+            cartState: true
+        }))
+    }
+
+
     const onNavScroll = () => {
         if (window.scrollY > 30) {
             setNavState(true);
@@ -46,7 +57,7 @@ const Navbar = () => {
                         </li>
 
                         <li className='grid items-center'>
-                            <button type='button' className='border-none outline-none active:scale-110 transition-all duration-300 relative'>
+                            <button type='button' onClick={onCartToggle} className='border-none outline-none active:scale-110 transition-all duration-300 relative'>
                                 <ShoppingBagIcon className={`icon-style ${navState && "text-slate-900 transition-all duration-300"}`} />
                                 <div className={`absolute top-4 right-0 bg-white text-slate-900 shadow shadow-slate-100 w-4 h-4 text-[0.65rem] leading-tight font-medium rounded-full flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-300 `}>0</div>
                             </button>
