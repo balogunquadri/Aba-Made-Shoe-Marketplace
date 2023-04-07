3de41359e06297bd129b4616c9e992ecfb35b7a8
Phantonce <yes201045@gmail.com>
2023-04-07 10:58:49 +0100

    Store Added

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -15,10 +15,13 @@ import CartEmpty from "./cart/CartEmpty"
 import CartItem from "./cart/CartItem"
 
 const Cart = () => {
-
-
     const dispatch = useDispatch();
     const ifCartState = useSelector(selectCartState);
+    const cartItems = useSelector(selectCartItems);
+    const totalAmount = useSelector(selectTotalAmount);
+    const totalQTY = useSelector(selectTotalQTY);
+
+
 
     const onCartToggle = () => {
         dispatch(setCloseCart({
@@ -34,15 +37,30 @@ const Cart = () => {
                 }`}
             >
 
-                <div className={`blur-effect-theme h-screen duration-500 max-w-xl w-full absolute right-0`}>
-                    <CartCount onCartToggle={onCartToggle} />
-                    <CartEmpty />
-                    <CartItem />
-                </div>
+                <CartCount totalQTY={totalQTY} onCartToggle={onCartToggle} onClearCartItems={onClearCartItems} />
+                {cartItems ?.length === 0 ? <CartEmpty onCartToggle={onCartToggle} /> : <div>
+                    <div className="flex items-start justify-start flex-col gap-y-7 lg:gap-y-5 overflow-y-scroll h-[81vh] scroll-smooth scroll-hidden py-3">
+                        {cartItems ?.map((item, i) => (
+                            <CartItem key={i} item={item} />
+                        ))}
+                    </div>
 
+                    <div className="fixed bottom-0 bg-white w-full px-5 py-2 grid items-center">
+                        <div className="flex items-center justify-between">
+                            <h1 className="text-base font-semibold uppercase">SubTotal</h1>
+                            <h1 className="text-sm rounded bg-theme-cart text-slate-100 px-1 py-0.5">${totalAmount}</h1>
+                        </div>
+                        <div className="grid items-center gap-2">
+                            <p className="text-sm font-medium text-center">Taxes and Shipping Will Calculate At Shipping</p>
+                            <button type="button" className="button-theme bg-theme-cart text-white">Check Out</button>
+                        </div>
+                    </div>
+
+                </div>}
             </div>
-        </>
-    )
-}
 
-export default Cart
+        </>
+    );
+};
+
+export default Cart;
