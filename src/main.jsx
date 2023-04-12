209196aca9a9c330552ac4765c5d7325a562cd75
Phantonce <yes201045@gmail.com>
2023-04-12 14:55:25 +0100

    last  edit

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { Provider } from 'react-redux'
-import Store from './app/Store.js';
-import { Toaster } from 'react-hot-toast'
-
+import { Provider } from "react-redux";
+import Store from './app/Store';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -13,5 +12,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Toaster position='top-center' reverseOrder={false} />
       <App />
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
